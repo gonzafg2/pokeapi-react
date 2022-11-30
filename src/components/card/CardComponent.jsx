6e57a24a0e75130582c3c38f82c3de2cb39c8538
Gonzalo Fleming <gonzafg2@gmail.com>
2022-11-30 16:48:36 -0300

    feat: :sparkles: Show pokemon data in DOM

--- src/components/card/CardComponent.jsx
+++ src/components/card/CardComponent.jsx
@@ -1,25 +1,31 @@
-import { useEffect } from "react";
-import axios from 'axios'
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const URL = "https://pokeapi.co/api/v2/pokemon/ditto";
 
-const requestPokemon = async (poke) => { 
+const requestPokemon = async (poke) => {
   try {
-    const req = await axios.get(poke)
-    console.log(req.data)
-    return req.data
+    const req = await axios.get(poke);
+    console.log(req.data);
+    return req.data;
   } catch (error) {
     console.error(error);
   }
-}
+};
 
 const CardComponent = () => {
+  const [pokemon, setPokemon] = useState(null);
 
   useEffect(() => {
-    requestPokemon(URL)
-  })
+    requestPokemon(URL).then((data) => setPokemon(data));
+  }, []);
 
-  return <h1>Vite + React Holi</h1>;
-}
+  return (
+    <div>
+      <h1>{pokemon?.name}</h1>
+      <img src={pokemon?.sprites.front_default} alt={pokemon?.name} />
+    </div>
+  );
+};
 
 export default CardComponent;
