6d3d27d6f09ff05d6674eecd5fbfec8570f28302
Gonzalo Fleming <gonzafg2@gmail.com>
2022-11-30 13:56:35 -0300

    feat: Connect to pokeAPI

--- src/components/card/CardComponent.jsx
+++ src/components/card/CardComponent.jsx
@@ -1,4 +1,24 @@
+import { useEffect } from "react";
+import axios from 'axios'
+
+const URL = "https://pokeapi.co/api/v2/pokemon/ditto";
+
+const requestPokemon = async (poke) => { 
+  try {
+    const req = await axios.get(poke)
+    console.log(req.data)
+    return req.data
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 const CardComponent = () => {
+
+  useEffect(() => {
+    requestPokemon(URL)
+  })
+
   return <h1>Vite + React Holi</h1>;
 }
 
